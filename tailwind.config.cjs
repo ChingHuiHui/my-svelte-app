e814db636024cea2cc6ceb4f432e877ec2a4fc8b
chinghuihui <sunny70624@gmail.com>
2022-08-10 11:50:03 +0800

    init by hui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{svelte,js,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/line-clamp')],
 }
