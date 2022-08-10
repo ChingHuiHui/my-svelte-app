47f001608e7ef1a0b41f7f3d440e54dfb3124e92
chinghuihui <sunny70624@gmail.com>
2022-08-10 12:04:05 +0800

    add cards in App

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,32 @@
-<div class="container my-20 text-center">
-  <h1 class="text-blue-500 text-3xl">Hello World</h1>
+<div class="container my-20">
+  <div class="grid grid-cols-4 gap-6">
+    <div
+      class="p-4 bg-gray-200 rounded-lg h-32 cursor-pointer hover:border-4 hover:border-teal-600"
+    >
+      <h2 class="text-2xl font-bold">title</h2>
+      <div class="text-lg">name</div>
+      <div class="text-sm">time</div>
+    </div>
+    <div
+      class="p-4 bg-gray-200 rounded-lg h-32 cursor-pointer hover:border-4 hover:border-teal-600"
+    >
+      <h2 class="text-2xl font-bold">title</h2>
+      <div class="text-lg">name</div>
+      <div class="text-sm">time</div>
+    </div>
+    <div
+      class="p-4 bg-gray-200 rounded-lg h-32 cursor-pointer hover:border-4 hover:border-teal-600"
+    >
+      <h2 class="text-2xl font-bold">title</h2>
+      <div class="text-lg">name</div>
+      <div class="text-sm">time</div>
+    </div>
+    <div
+      class="p-4 bg-gray-200 rounded-lg h-32 cursor-pointer hover:border-4 hover:border-teal-600"
+    >
+      <h2 class="text-2xl font-bold">title</h2>
+      <div class="text-lg">name</div>
+      <div class="text-sm">time</div>
+    </div>
+  </div>
 </div>
